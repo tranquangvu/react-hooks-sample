6d4f75ee3114a09248e7bf077b0386ce27f5d122
tranquangvu <vutran.513@gmail.com>
2020-04-07 16:09:37 +0700

    Add book form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BookContextProvider from './contexts/BookContext';
 import NavBar from './components/NavBar';
 import BookList from './components/BookList';
+import BookForm from './components/BookForm';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BookContextProvider>
         <NavBar />
         <BookList />
+        <BookForm />
       </BookContextProvider>
     </div>
   );
--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,10 +1,25 @@
-import React, {} from 'react';
+import React, { useContext, useState } from 'react';
+import { BookContext } from '../contexts/BookContext';
 
 const BookForm = () => {
+  const { addBook } = useContext(BookContext);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addBook(title, author);
+    setTitle('');
+    setAuthor('');
+  }
+
   return (
-    <div className="book-form">
-      Book form
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Book title" required value={title} onChange={(e) => setTitle(e.target.value)} />
+      <input type="text" placeholder="Book author" required value={author} onChange={(e) => setAuthor(e.target.value)} />
+      <input type="submit" value="Add book" />
+    </form>
   )
 }
 
